b10e54e8031990933d3d1e6b626098e36989612a
Joshua Dolor <dolorjsh@gmail.com>
2019-01-12 18:18:44 +0800

    minifying of html in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const folderPath = 'dist/src/js';
+const htmlFolderPath = 'dist';
+
+const isDev = process.env.NODE_ENV === 'development';
+const minifyHTML = !isDev;
 
 module.exports = {
     entry: './resources/js/app.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, folderPath),
         filename: 'main.js',
     },
     module: {
-    rules: [
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        }
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: path.resolve(__dirname, htmlFolderPath+'/index.html'),
+            template: path.resolve(__dirname, 'resources/index.html'),
+            chunks: ['main'],
+            minify   : {
+                html5                          : minifyHTML,
+                collapseWhitespace             : minifyHTML,
+                minifyCSS                      : minifyHTML,
+                minifyJS                       : minifyHTML,
+                minifyURLs                     : false,
+                removeAttributeQuotes          : minifyHTML,
+                removeComments                 : minifyHTML,
+                removeEmptyAttributes          : minifyHTML,
+                removeOptionalTags             : minifyHTML,
+                removeRedundantAttributes      : minifyHTML,
+                removeScriptTypeAttributes     : minifyHTML,
+                removeStyleLinkTypeAttributese : minifyHTML,
+                useShortDoctype                : minifyHTML
+              }
+        }),
     ]
-}
 };
