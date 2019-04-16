77c2c248465e5ce70fdcd0e7a90b19956c627f5f
anna rose <annrose.roseann2194@gmail.com>
2019-04-16 14:52:29 +0800

    parallax scroll

--- resources/js/Framework/Components/App.js
+++ resources/js/Framework/Components/App.js
@@ -2,8 +2,10 @@ import Config from '~/Application/Config';
 import Page from '~/Framework/Components/Page';
 import NavBtn from '~/Framework/Components/NavButton';
 import Toolbar from '~/Framework/Components/Toolbar';
-
 import Hooks from '~/Framework/Helpers/hooks';
+import Parallax from 'parallax-js/dist/parallax.min.js';
+
+
 class App {
     
     constructor() {
@@ -24,8 +26,14 @@ class App {
         //last function
         this.removePreloader();
 
-    }
+        // parallax
+        var scene = document.getElementById('activate_scene');
+        var parallaxInstance = new Parallax(scene, {
+          relativeInput: true
+        });
 
+    }
+ 
     onResize() {
         $(window).resize(() => {
             this.getInitialDimensions();
