3c5bdd2206ba37a8a0e4860a872556ddae4b7f28
anna rose <annrose.roseann2194@gmail.com>
2019-07-30 15:46:22 +0800

    add testimonials | create svg type animated logo

--- resources/js/Application/Config/index.js
+++ resources/js/Application/Config/index.js
@@ -210,7 +210,7 @@ export default {
     engage: {
         thought_balloons: [
             {
-                message: "“Awesome time at the #NBA3XPH2018 celebrity games (emoji) One of the longest and best running yearly events…”",
+                message: "“First love. &#127936 Awesome time at the #NBA3XPH2018 celebrity games &#128175 One of the longest and best running yearly events out there. Shout everyone who played! Will definitely be back next...”",
                 className: "balloon balloon-p1"
             },
             {
@@ -218,12 +218,72 @@ export default {
                 className: "balloon balloon-p2"
             },
             {
-                message: "Happy Mother’s Day to all Super Moms! #MothersDay2018 #eastwoodcity #MumForMoms #LazadaSuperMoms”",
+                message: "Happy Mother’s Day to all Super Moms! <br> #MothersDay2018 #eastwoodcity #MumForMoms #LazadaSuperMoms”",
                 className: "balloon balloon-p3"
             },
             {
-                message: "“Thank you @lazadaph for these Mums for Moms! #LazadaPh #MumsForMoms”",
+                message: "“Thank you @lazadaph for these Mums for Moms! <br> #LazadaPh #MumsForMoms”",
                 className: "balloon balloon-p4"
+            },
+            {
+                message: "“Point to point. Free Ride just for moms! <br> LazadaPHSuperMoms #JustForMoms”",
+                className: "balloon balloon-p5"
+            },
+            {
+                message: "“All set for tonight’s Grand Finale Party. Rock N’Roll ACC 2018. <br> #ACC2018 #EyesOn2020”",
+                className: "balloon balloon-p1"
+            },
+            {
+                message: "“Nissan #Terra” makes Philippines debut in sunset launch.”",
+                className: "balloon balloon-p3"
+            },
+            {
+                message: "“The Turbo Zone team took the new Nissan Terra for a drive! During the unveiling, it’s emphasized to be a tough vehicle that delivers smooth ride, regardless of situation.”",
+                className: "balloon balloon-p5"
+            },
+            {
+                message: "“New @Nissan Terra officially unveiled in the Philippines… Miniature Terra on a hill not included. &#128517 <br> #TheNewNissanTerra #ExceptionalEveryday”",
+                className: "balloon balloon-p1"
+            },
+            {
+                message: "“Last night was CRAZY!!!!! Always an amazing party with @bacardi &#127867 <br> #Pobrasyon #BacardiHouseParty”",
+                className: "balloon balloon-p3"
+            },
+            {
+                message: "Second time doing a #pubcrawl in Poblacion… loving everything so far. &#128375 &#9904 &#128128.",
+                className: "balloon balloon-p3"
+            },
+            {
+                message: "“Congrats to your first ever house party, Lazada! &#129303 Bestpart? Yun pwede mo gusto ung gusto mo kahit may limit na 20 items lang. &#128514”",
+                className: "balloon balloon-p2"
+            },
+            {
+                message: "“Wala din kaming ligtas!!! #wattahwattah2018”",
+                className: "balloon balloon-p4"
+            },
+            {
+                message: "“So this happened last night! &#128557 So lucky to have shared the same platform with her. I couldn’t believe this really happened. It was one for the books!!! &#10084 <br> #FutureMobility #Toyota #PIMS2018”",
+                className: "balloon balloon-p1"
+            },
+            {
+                message: "“Thank you @toyotamotorphilippines! It was a great event. <br> #Toyota7thPIMS #EverMobilityForAll”",
+                className: "balloon balloon-p3"
+            },
+            {
+                message: "“The Year-End Bigatin Sales Force Appreciation Night. &#127881 #TeamToyotaGreatGatsbi”",
+                className: "balloon balloon-p5"
+            },
+            {
+                message: "<h6>#FXMedPH</h6>",
+                className: "balloon balloon-p1"
+            },
+            {
+                message: "<h6>#Pobrasyon</h6>",
+                className: "balloon balloon-p4"
+            },
+            {
+                message: "<h6>#WattahWattah2018</h6>",
+                className: "balloon balloon-p3"
             },
         ],
         live_stream_video: '<iframe src="https://www.youtube.com/embed/xcJtL7QggTI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
